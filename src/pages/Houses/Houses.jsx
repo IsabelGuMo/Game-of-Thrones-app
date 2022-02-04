dcbb8c035449a007b8bc5f06a1557ca98e99855d
Jonathan <noni-06@hotmail.com>
2022-02-04 15:52:21 +0100

    searcher houses

--- src/pages/Houses/Houses.jsx
+++ src/pages/Houses/Houses.jsx
@@ -2,20 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { getHouses } from '../../api/fetchToApi';
 import HouseCard from '../../components/HouseCard/HouseCard';
 import "./Houses.scss"
+import Search from "./../../components/Searcher/Searcher.jsx";
 
-
-const Houses = () => {
+const Houses = ({props}) => {
   const [houses, setHouses] = useState([]);
 
   useEffect(() => {
     getHouses().then((data) => {
-      setHouses(data);
+      const filteredHouses = data.filter((item) => {
+        return item.name.toLowerCase().includes(props.search);
+      });
+      setHouses(filteredHouses);
     });
-  }, []);
+  }, [props.search]);
 
   return (
     <div>
       <h2>Houses</h2>
+      <Search props={props}/>
       <div className="houses-container">
       {houses.map((house) => (
       <HouseCard key={house.name} house={house}/>
